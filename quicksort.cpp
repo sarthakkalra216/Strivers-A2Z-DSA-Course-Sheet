9a0ff62aa78a61789c2d6141e7097b1a0191e9c0
Sarthak Kalra <102536154+sarthakkalra216@users.noreply.github.com>
2023-07-08 14:15:20 +0530

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -17,36 +17,37 @@ class Solution
 {
     public:
     //Function to sort an array using quick sort algorithm.
-    void qst(int arr[], int low, int high)
+     void quickSort(int arr[], int low, int high)
     {
         if (low < high) {
-        int pIndex = partition(arr, low, high);
-        qs(arr, low, pIndex - 1);
-        qs(arr, pIndex + 1, high);
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
     }
-        
-        
-    }
-    
-    public:
-    int partition (int arr[], int low, int high)
+
+    int partition(int arr[], int low, int high)
     {
-     int pivot = arr[low];
-    int i = low;
-    int j = high;
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
 
-    while (i < j) {
-        while (arr[i] <= pivot && i <= high - 1) {
-            i++;
+        while (i <= j) {
+            while (arr[i] <= pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot ) {
+                j--;
+            }
+
+            if (i < j) {
+                swap(arr[i], arr[j]);
+            }
         }
 
-        while (arr[j] > pivot && j >= low + 1) {
-            j--;
-        }
-        if (i < j) swap(arr[i], arr[j]);
-    }
-    swap(arr[low], arr[j]);
-    return j;
+        swap(arr[low], arr[j]);
+        return j;
     }
 };
 
