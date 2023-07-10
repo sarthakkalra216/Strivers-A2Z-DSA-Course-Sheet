e6c9fe01f2ad57a33d98fbdef310b1ffbe47ad18
Sarthak Kalra <102536154+sarthakkalra216@users.noreply.github.com>
2023-07-10 11:04:38 +0530

    Create twosum.cpp

---
+++ twosum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      map<int,int> mp;
+      int n=nums.size();
+
+      for(int i =0;i<n;i++)
+      { 
+          int num=nums[i];
+          int more=target-num;
+          if(mp.find(more)!=mp.end())
+            {
+              return {i,mp[more]};
+            }
+        mp[num]=i;
+      }
+    return {-1,-1};
+    }
+};
